be49b5f853401e41f981dff8be51feff32621804
Hamza Sakr <hmzskr@gmail.com>
2019-06-26 21:03:04 -0700

    format files

--- static/js/map.js
+++ static/js/map.js
@@ -2,7 +2,7 @@ var map = L.map("map", {
   style: 'mapbox://styles/mapbox/dark-v10',
   center: [39.5, -98.35], // starting position
   zoom: 5 // starting zoom
-  });
+});
 
 // Adding tile layer
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -16,12 +16,12 @@ var link = "https://raw.githubusercontent.com/tpj728/Project_2/master/state_car_
 
 function chooseColor(body) {
   switch (body) {
-  case "Truck":
-    return "lightcoral";
-  case "SUV/Crossover":
-    return "yellow";
-  default:
-    return "lightgreen";
+      case "Truck":
+          return "lightcoral";
+      case "SUV/Crossover":
+          return "yellow";
+      default:
+          return "lightgreen";
   }
 }
 
@@ -29,51 +29,51 @@ function chooseColor(body) {
 d3.json(link, function(data) {
   // Creating a geoJSON layer with the retrieved data
   L.geoJson(data, {
-    // Style each feature (state)
-    style: function(feature) {
-      return {
-        color: "grey",
-        // Call the chooseColor function to decide state color (color based on vehicle body style)
-        // fillColor: "lightblue",
-        fillColor: chooseColor(feature.properties.BODY),
-        fillOpacity: 0.3,
-        weight: 1.5
-      };
-    },
-    // Called on each feature
-    onEachFeature: function(feature, layer) {
-      // Set mouse events to change map styling
-      layer.on({
-        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 60% so that it stands out
-        mouseover: function(event) {
-          layer = event.target;
-          layer.setStyle({
-            fillOpacity: 0.6
+      // Style each feature (state)
+      style: function(feature) {
+          return {
+              color: "grey",
+              // Call the chooseColor function to decide state color (color based on vehicle body style)
+              // fillColor: "lightblue",
+              fillColor: chooseColor(feature.properties.BODY),
+              fillOpacity: 0.3,
+              weight: 1.5
+          };
+      },
+      // Called on each feature
+      onEachFeature: function(feature, layer) {
+          // Set mouse events to change map styling
+          layer.on({
+              // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 60% so that it stands out
+              mouseover: function(event) {
+                  layer = event.target;
+                  layer.setStyle({
+                      fillOpacity: 0.6
+                  });
+              },
+              // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 30%
+              mouseout: function(event) {
+                  layer = event.target;
+                  layer.setStyle({
+                      fillOpacity: 0.3
+                  });
+              },
+              // When a feature (state) is clicked, it is enlarged to fit the screen
+              click: function(event) {
+                  // map.fitBounds(event.target.getBounds());
+                  map.setView(event.latlng, 5.5);
+              }
           });
-        },
-        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 30%
-        mouseout: function(event) {
-          layer = event.target;
-          layer.setStyle({
-            fillOpacity: 0.3
-          });
-        },
-        // When a feature (state) is clicked, it is enlarged to fit the screen
-        click: function(event) {
-          // map.fitBounds(event.target.getBounds());
-          map.setView(event.latlng, 5.5);
-        }
-      });
-      // Giving each feature a pop-up with information pertinent to it
-      layer.bindPopup(`<div id="marker-panel"><h1> ${feature.properties.NAME} </h1><hr><br>
-      	<a href="https://www.motortrend.com/cars/${feature.properties.MAKE}/${feature.properties.MODEL}/2016/" target="_blank">
-      	<img src = "${feature.properties.IMGURL}" id="car-image"></a>
-      	<div id = "marker-panel-text">
-      	<br> <b>Make:</b> ${feature.properties.MAKE} <br> <b>Model:</b> ${feature.properties.MODEL}
-      	<br> <b>MSRP:</b> ${feature.properties.MSRP} <br> <b>Class:</b> ${feature.properties.CLASS} 
-      	<br> <b>MPG:</b> ${feature.properties.MPG} <br> <b>Body:</b> ${feature.properties.BODY}<div></div>`);
+          // Giving each feature a pop-up with information pertinent to it
+          layer.bindPopup(`<div id="marker-panel"><h1> ${feature.properties.NAME} </h1><hr><br>
+    <a href="https://www.motortrend.com/cars/${feature.properties.MAKE}/${feature.properties.MODEL}/2016/" target="_blank">
+    <img src = "${feature.properties.IMGURL}" id="car-image"></a>
+    <div id = "marker-panel-text">
+    <br> <b>Make:</b> ${feature.properties.MAKE} <br> <b>Model:</b> ${feature.properties.MODEL}
+    <br> <b>MSRP:</b> ${feature.properties.MSRP} <br> <b>Class:</b> ${feature.properties.CLASS} 
+    <br> <b>MPG:</b> ${feature.properties.MPG} <br> <b>Body:</b> ${feature.properties.BODY}<div></div>`);
 
-    }
+      }
   }).addTo(map);
 
 });
--- static/js/plots.js
+++ static/js/plots.js
@@ -1,77 +1,81 @@
 //Google Charts
-google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.load('current', {
+  packages: ['corechart', 'bar']
+});
 google.charts.setOnLoadCallback(drawBarColors);
 
 function drawBarColors() {
-      var data = google.visualization.arrayToDataTable(plotData);
+  var data = google.visualization.arrayToDataTable(plotData);
 
-      var options = {
-        title: 'Car Value Depreciation (2016 vs 2019)',
-        chartArea: {width: '50%'},
-        colors: ['#262CB0', '#B02626'],
-        hAxis: {
+  var options = {
+      title: 'Car Value Depreciation (2016 vs 2019)',
+      chartArea: {
+          width: '50%'
+      },
+      colors: ['#262CB0', '#B02626'],
+      hAxis: {
           title: 'Price ($)',
           minValue: 0
-        },
-        vAxis: {
+      },
+      vAxis: {
           title: 'Car'
-        },
-        height: 700
-      };
-      var chart = new google.visualization.BarChart(document.getElementById('bar_chart'));
-      chart.draw(data, options);
-    }
+      },
+      height: 700
+  };
+  var chart = new google.visualization.BarChart(document.getElementById('bar_chart'));
+  chart.draw(data, options);
+}
 
 
 // Plotly
 function plotlyInit() {
-    var data = [{
+  var data = [{
       values: modelValues,
       labels: modelLabels,
       hole: .4,
-      type: 'pie' 
-    }];
+      type: 'pie'
+  }];
 
-    var layout = {
-        title: 'Car Popularity by Model, Make, and Body Style',
-        height: 800,
-        width: 800
-      };    
-      
-    Plotly.newPlot('donut_chart', data, layout);
-  }
-  
+  var layout = {
+      title: 'Car Popularity by Model, Make, and Body Style',
+      height: 800,
+      width: 800
+  };
+
+  Plotly.newPlot('donut_chart', data, layout);
+}
+
 function updatePlotly(newV, newL) {
-  
-    Plotly.restyle('donut_chart', "values", [newV]);
-    Plotly.restyle('donut_chart', "labels", [newL]);
-  }
-  
+
+  Plotly.restyle('donut_chart', "values", [newV]);
+  Plotly.restyle('donut_chart', "labels", [newL]);
+}
+
 function getData(dataset) {
 
-    var values = [];
-    var labels = [];
-  
-    switch (dataset) {
-    case "modelset":
-      values = modelValues;
-      labels = modelLabels;
-      break;
-    case "makeset":
-      values = makeValues;
-      labels = makeLabels;
-      break;      
-    case "bodyset":
-      values = bodyValues;
-      labels = bodyLabels;
-      break;
-    default:
-      values = modelValues;
-      labels = modelLabels;
-      break;
-    }
-  
-    updatePlotly(values, labels);
+  var values = [];
+  var labels = [];
+
+  switch (dataset) {
+      case "modelset":
+          values = modelValues;
+          labels = modelLabels;
+          break;
+      case "makeset":
+          values = makeValues;
+          labels = makeLabels;
+          break;
+      case "bodyset":
+          values = bodyValues;
+          labels = bodyLabels;
+          break;
+      default:
+          values = modelValues;
+          labels = modelLabels;
+          break;
   }
-  
-  plotlyInit();
+
+  updatePlotly(values, labels);
+}
+
+plotlyInit();
